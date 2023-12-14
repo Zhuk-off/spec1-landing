026d1755532088a8fc859566a61418ede21cbc84
Alexander <zhukoff.alexander@gmail.com>
2023-12-14 13:45:58 +0300

    chore: Add fonts

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,7 +1,9 @@
 export async function HomePage() {
   return (
     <main className="">
-      <h1>Спец №1</h1>
+      <h1 className="text-5xl uppercase font-specTitle font-title text-yellow-700">
+        Спец №1
+      </h1>
     </main>
   );
 }
