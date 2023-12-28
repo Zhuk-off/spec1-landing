06449145fc262a1dc248282e02cbb0767b171e5a
Alexander <zhukoff.alexander@gmail.com>
2023-12-28 21:02:47 +0300

    fix: Fix nextconfig

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,63 +1,63 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    formats: [
-      'image/webp',
-      'image/avif', 
-    ],
-    domains: [
-      "localhost", 
-      "**.localhost", 
-      "zhukoff.by",
-      "spec.zhukoff.by", 
-      "spec1.zhukoff.by", 
-      "www.spec1.zhukoff.by", 
-    ]
-    // formats: ['image/avif', 'image/webp'],
-    // remotePatterns: [
-    //   // {
-    //   //   protocol: "http",
-    //   //   hostname: "localhost",
-    //   //   port: "1338",
-    //   //   pathname: "/uploads/**",
-    //   // },
-    //   {
-    //     protocol: "http",
-    //     hostname: "localhost",
-    //   },
-    //   {
-    //     protocol: "https",
-    //     hostname: "localhost",
-    //   },
-    //   {
-    //     protocol: 'https',
-    //     hostname: '**.zhukoff.by',
-    //     port: '',
-    //     pathname: '/**',
-    //   },
-    //   {
-    //     protocol: "http",
-    //     hostname: "localhost",
-    //   },
-    //   {
-    //     protocol: "http",
-    //     hostname: "**.zhukoff.by",
-    //   },
-    //   {
-    //     protocol: "https",
-    //     hostname: "**.zhukoff.by",
-    //   },
-    //   // {
-    //   //   protocol: "http",
-    //   //   hostname: "**.zhukoff.by",
-    //   // },
-    //   // {
-    //   //   protocol: "https",
-    //   //   hostname: "spec1.zhukoff.by",
-    //   //   port: "",
-    //   //   pathname: "/uploads/**",
-    //   // },
+    // formats: [
+    //   'image/webp',
+    //   'image/avif', 
     // ],
+    // domains: [
+    //   "localhost", 
+    //   "**.localhost", 
+    //   "zhukoff.by",
+    //   "spec.zhukoff.by", 
+    //   "spec1.zhukoff.by", 
+    //   "www.spec1.zhukoff.by", 
+    // ]
+    // formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      // {
+      //   protocol: "http",
+      //   hostname: "localhost",
+      //   port: "1338",
+      //   pathname: "/uploads/**",
+      // },
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      // {
+      //   protocol: "https",
+      //   hostname: "localhost",
+      // },
+      // {
+      //   protocol: 'https',
+      //   hostname: '**.zhukoff.by',
+      //   port: '',
+      //   pathname: '/**',
+      // },
+      // {
+      //   protocol: "http",
+      //   hostname: "localhost",
+      // },
+      // {
+      //   protocol: "http",
+      //   hostname: "**.zhukoff.by",
+      // },
+      {
+        protocol: "https",
+        hostname: "**.zhukoff.by",
+      },
+      // {
+      //   protocol: "http",
+      //   hostname: "**.zhukoff.by",
+      // },
+      // {
+      //   protocol: "https",
+      //   hostname: "spec1.zhukoff.by",
+      //   port: "",
+      //   pathname: "/uploads/**",
+      // },
+    ],
   },
 };
 
